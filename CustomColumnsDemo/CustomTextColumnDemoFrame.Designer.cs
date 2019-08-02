bd0e007a432cd4d17bf14b92d42abee22dffe054
Serge Kraikov <serge.kraikov@gmail.com>
2019-08-02 23:31:03 +0500

    3.5.24.1138

--- CustomColumnsDemo/CustomTextColumnDemoFrame.Designer.cs
+++ CustomColumnsDemo/CustomTextColumnDemoFrame.Designer.cs
@@ -28,34 +28,126 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CustomTextColumnDemoFrame));
-			this.queryBuilder1 = new ActiveQueryBuilder.View.WinForms.QueryBuilder();
-			this.SuspendLayout();
-			// 
-			// queryBuilder1
-			// 
-			this.queryBuilder1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.queryBuilder1.Location = new System.Drawing.Point(0, 0);
-			this.queryBuilder1.Name = "queryBuilder1";
-			this.queryBuilder1.PanesConfigurationOptions.DatabaseSchemaViewWidth = 230;
-			this.queryBuilder1.Size = new System.Drawing.Size(640, 340);
-			this.queryBuilder1.TabIndex = 2;
-			this.queryBuilder1.QueryElementControlCreated += new ActiveQueryBuilder.View.QueryView.QueryElementControlCreatedEventHandler(this.queryBuilder1_QueryElementControlCreated);
-			this.queryBuilder1.QueryElementControlDestroying += new ActiveQueryBuilder.View.QueryView.QueryElementControlDestroyingEventHandler(this.queryBuilder1_QueryElementControlDestroying);
-			// 
-			// CustomTextColumnDemoFrame
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Controls.Add(this.queryBuilder1);
-			this.Name = "CustomTextColumnDemoFrame";
-			this.Size = new System.Drawing.Size(640, 340);
-			this.ResumeLayout(false);
+            ActiveQueryBuilder.View.WinForms.QueryView.LinkPainterAccess linkPainterAccess1 = new ActiveQueryBuilder.View.WinForms.QueryView.LinkPainterAccess();
+            this.queryBuilder1 = new ActiveQueryBuilder.View.WinForms.QueryBuilder();
+            ((System.ComponentModel.ISupportInitialize)(this.queryBuilder1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.queryBuilder1.SQLFormattingOptions)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.queryBuilder1.SQLGenerationOptions)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // queryBuilder1
+            // 
+            this.queryBuilder1.AddObjectDialogOptions.Location = new System.Drawing.Point(0, 0);
+            this.queryBuilder1.AddObjectDialogOptions.Size = new System.Drawing.Size(430, 430);
+            this.queryBuilder1.AddObjectDialogOptions.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.queryBuilder1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.queryBuilder1.DatabaseSchemaViewOptions.DrawTreeLines = false;
+            this.queryBuilder1.DatabaseSchemaViewOptions.ImageList = null;
+            this.queryBuilder1.DesignPaneOptions.Background = System.Drawing.SystemColors.Window;
+            linkPainterAccess1.LinkColor = System.Drawing.Color.Black;
+            linkPainterAccess1.LinkColorFocused = System.Drawing.Color.Black;
+            linkPainterAccess1.MarkColor = System.Drawing.SystemColors.Control;
+            linkPainterAccess1.MarkColorFocused = System.Drawing.SystemColors.ControlDark;
+            linkPainterAccess1.MarkStyle = ActiveQueryBuilder.View.QueryView.LinkMarkStyle.Access;
+            this.queryBuilder1.DesignPaneOptions.LinkPainterOptions = linkPainterAccess1;
+            this.queryBuilder1.DesignPaneOptions.LinkStyle = ActiveQueryBuilder.View.QueryView.LinkStyle.MSAccess;
+            this.queryBuilder1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.queryBuilder1.Location = new System.Drawing.Point(0, 0);
+            this.queryBuilder1.Name = "queryBuilder1";
+            this.queryBuilder1.PanesConfigurationOptions.DatabaseSchemaViewWidth = 230;
+            this.queryBuilder1.PanesConfigurationOptions.PropertiesBarDockOptions.AutoHide = true;
+            this.queryBuilder1.PanesConfigurationOptions.PropertiesBarDockOptions.Position = ActiveQueryBuilder.View.SidePanelDockStyle.Right;
+            this.queryBuilder1.PanesConfigurationOptions.PropertiesBarEnabled = true;
+            this.queryBuilder1.PanesConfigurationOptions.SubQueryNavBarDockOptions.AutoHide = true;
+            this.queryBuilder1.PanesConfigurationOptions.SubQueryNavBarDockOptions.Position = ActiveQueryBuilder.View.SidePanelDockStyle.Left;
+            this.queryBuilder1.PanesConfigurationOptions.SubQueryNavBarEnabled = true;
+            this.queryBuilder1.QueryColumnListOptions.ColumnsOptions.AggregateColumn.Index = 5;
+            this.queryBuilder1.QueryColumnListOptions.ColumnsOptions.AggregateColumn.Width = 90D;
+            this.queryBuilder1.QueryColumnListOptions.ColumnsOptions.AliasColumn.Index = 2;
+            this.queryBuilder1.QueryColumnListOptions.ColumnsOptions.AliasColumn.Width = 100D;
+            this.queryBuilder1.QueryColumnListOptions.ColumnsOptions.ConditionTypeColumn.Index = 7;
+            this.queryBuilder1.QueryColumnListOptions.ColumnsOptions.ConditionTypeColumn.Width = 140D;
+            this.queryBuilder1.QueryColumnListOptions.ColumnsOptions.CriteriaColumn.Index = 8;
+            this.queryBuilder1.QueryColumnListOptions.ColumnsOptions.CriteriaColumn.Width = 200D;
+            this.queryBuilder1.QueryColumnListOptions.ColumnsOptions.CriteriaOrColumns.Index = 0;
+            this.queryBuilder1.QueryColumnListOptions.ColumnsOptions.CriteriaOrColumns.Width = 60D;
+            this.queryBuilder1.QueryColumnListOptions.ColumnsOptions.ExpressionColumn.Index = 1;
+            this.queryBuilder1.QueryColumnListOptions.ColumnsOptions.ExpressionColumn.Width = 250D;
+            this.queryBuilder1.QueryColumnListOptions.ColumnsOptions.GroupingColumn.Index = 6;
+            this.queryBuilder1.QueryColumnListOptions.ColumnsOptions.GroupingColumn.Width = 100D;
+            this.queryBuilder1.QueryColumnListOptions.ColumnsOptions.OutputColumn.Index = 0;
+            this.queryBuilder1.QueryColumnListOptions.ColumnsOptions.OutputColumn.Width = 55D;
+            this.queryBuilder1.QueryColumnListOptions.ColumnsOptions.SortOrderColumn.Index = 4;
+            this.queryBuilder1.QueryColumnListOptions.ColumnsOptions.SortOrderColumn.Width = 100D;
+            this.queryBuilder1.QueryColumnListOptions.ColumnsOptions.SortTypeColumn.Index = 3;
+            this.queryBuilder1.QueryColumnListOptions.ColumnsOptions.SortTypeColumn.Width = 60D;
+            this.queryBuilder1.QueryColumnListOptions.Font = null;
+            this.queryBuilder1.QueryColumnListOptions.InitialOrColumnsCount = 2;
+            this.queryBuilder1.QueryColumnListOptions.NullOrderingInOrderBy = false;
+            this.queryBuilder1.QueryColumnListOptions.UseCustomExpressionBuilder = ActiveQueryBuilder.View.QueryView.AffectedColumns.None;
+            this.queryBuilder1.QueryNavBarOptions.CTEButtonBaseColor = System.Drawing.Color.Green;
+            this.queryBuilder1.QueryNavBarOptions.DisableQueryNavigationBarPopup = false;
+            this.queryBuilder1.QueryNavBarOptions.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.queryBuilder1.QueryNavBarOptions.OverflowButtonBaseColor = System.Drawing.Color.DarkRed;
+            this.queryBuilder1.QueryNavBarOptions.RootQueryButtonBaseColor = System.Drawing.Color.Black;
+            this.queryBuilder1.QueryNavBarOptions.SubQueryButtonBaseColor = System.Drawing.Color.Blue;
+            this.queryBuilder1.Size = new System.Drawing.Size(640, 340);
+            // 
+            // 
+            // 
+            this.queryBuilder1.SQLFormattingOptions.CTESubQueryFormat.FromClauseFormat.JoinConditionFormat.NewLineBefore = ActiveQueryBuilder.Core.SQLBuilderConditionFormatNewLine.None;
+            this.queryBuilder1.SQLFormattingOptions.CTESubQueryFormat.HavingFormat.NewLineBefore = ActiveQueryBuilder.Core.SQLBuilderConditionFormatNewLine.None;
+            this.queryBuilder1.SQLFormattingOptions.CTESubQueryFormat.WhereFormat.NewLineBefore = ActiveQueryBuilder.Core.SQLBuilderConditionFormatNewLine.None;
+            this.queryBuilder1.SQLFormattingOptions.DerivedQueryFormat.FromClauseFormat.JoinConditionFormat.NewLineBefore = ActiveQueryBuilder.Core.SQLBuilderConditionFormatNewLine.None;
+            this.queryBuilder1.SQLFormattingOptions.DerivedQueryFormat.HavingFormat.NewLineBefore = ActiveQueryBuilder.Core.SQLBuilderConditionFormatNewLine.None;
+            this.queryBuilder1.SQLFormattingOptions.DerivedQueryFormat.WhereFormat.NewLineBefore = ActiveQueryBuilder.Core.SQLBuilderConditionFormatNewLine.None;
+            this.queryBuilder1.SQLFormattingOptions.DynamicIndents = false;
+            this.queryBuilder1.SQLFormattingOptions.DynamicRightMargin = false;
+            this.queryBuilder1.SQLFormattingOptions.ExpandVirtualFields = false;
+            this.queryBuilder1.SQLFormattingOptions.ExpandVirtualObjects = false;
+            this.queryBuilder1.SQLFormattingOptions.ExpressionSubQueryFormat.FromClauseFormat.JoinConditionFormat.NewLineBefore = ActiveQueryBuilder.Core.SQLBuilderConditionFormatNewLine.None;
+            this.queryBuilder1.SQLFormattingOptions.ExpressionSubQueryFormat.HavingFormat.NewLineBefore = ActiveQueryBuilder.Core.SQLBuilderConditionFormatNewLine.None;
+            this.queryBuilder1.SQLFormattingOptions.ExpressionSubQueryFormat.WhereFormat.NewLineBefore = ActiveQueryBuilder.Core.SQLBuilderConditionFormatNewLine.None;
+            this.queryBuilder1.SQLFormattingOptions.MainQueryFormat.FromClauseFormat.JoinConditionFormat.NewLineBefore = ActiveQueryBuilder.Core.SQLBuilderConditionFormatNewLine.None;
+            this.queryBuilder1.SQLFormattingOptions.MainQueryFormat.HavingFormat.NewLineBefore = ActiveQueryBuilder.Core.SQLBuilderConditionFormatNewLine.None;
+            this.queryBuilder1.SQLFormattingOptions.MainQueryFormat.WhereFormat.NewLineBefore = ActiveQueryBuilder.Core.SQLBuilderConditionFormatNewLine.None;
+            // 
+            // 
+            // 
+            this.queryBuilder1.SQLGenerationOptions.ExpandVirtualFields = true;
+            this.queryBuilder1.SQLGenerationOptions.ExpandVirtualObjects = true;
+            this.queryBuilder1.TabIndex = 2;
+            this.queryBuilder1.VisualOptions.ActiveDockPanelCaptionColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(242)))), ((int)(((byte)(157)))));
+            this.queryBuilder1.VisualOptions.ActiveDockPanelCaptionFontColor = System.Drawing.Color.Black;
+            this.queryBuilder1.VisualOptions.DockTabColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(57)))), ((int)(((byte)(85)))));
+            this.queryBuilder1.VisualOptions.DockTabFontColor = System.Drawing.Color.White;
+            this.queryBuilder1.VisualOptions.DockTabFontHoverColor = System.Drawing.Color.White;
+            this.queryBuilder1.VisualOptions.DockTabHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(57)))), ((int)(((byte)(85)))));
+            this.queryBuilder1.VisualOptions.DockTabIndicatorColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(90)))), ((int)(((byte)(125)))));
+            this.queryBuilder1.VisualOptions.DockTabIndicatorHoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(167)))), ((int)(((byte)(183)))));
+            this.queryBuilder1.VisualOptions.InactiveDockPanelCaptionColor = System.Drawing.Color.FromArgb(((int)(((byte)(77)))), ((int)(((byte)(96)))), ((int)(((byte)(130)))));
+            this.queryBuilder1.VisualOptions.InactiveDockPanelCaptionFontColor = System.Drawing.Color.White;
+            this.queryBuilder1.VisualOptions.TabFont = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.queryBuilder1.VisualOptions.TabsStripBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(57)))), ((int)(((byte)(85)))));
+            this.queryBuilder1.QueryElementControlCreated += new ActiveQueryBuilder.View.QueryView.QueryElementControlCreatedEventHandler(this.queryBuilder1_QueryElementControlCreated);
+            this.queryBuilder1.QueryElementControlDestroying += new ActiveQueryBuilder.View.QueryView.QueryElementControlDestroyingEventHandler(this.queryBuilder1_QueryElementControlDestroying);
+            // 
+            // CustomTextColumnDemoFrame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.queryBuilder1);
+            this.Name = "CustomTextColumnDemoFrame";
+            this.Size = new System.Drawing.Size(640, 340);
+            ((System.ComponentModel.ISupportInitialize)(this.queryBuilder1.SQLFormattingOptions)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.queryBuilder1.SQLGenerationOptions)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.queryBuilder1)).EndInit();
+            this.ResumeLayout(false);
 
 		}
 
 		#endregion
 
 		private ActiveQueryBuilder.View.WinForms.QueryBuilder queryBuilder1;
-	}
+    }
 }
--- CustomColumnsDemo/CustomTextColumnDemoFrame.cs
+++ CustomColumnsDemo/CustomTextColumnDemoFrame.cs
@@ -11,40 +11,143 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
-
 using ActiveQueryBuilder.Core;
 using ActiveQueryBuilder.View.QueryView;
 
 namespace CustomColumnsDemo
 {
-	public partial class CustomTextColumnDemoFrame : UserControl
-	{
-		private readonly List<string> _customValuesProvider = new List<string>();
+    public partial class CustomTextColumnDemoFrame : UserControl
+    {
         private DataGridViewTextBoxColumn _customColumn;
 
+        private readonly Dictionary<UnionSubQuery, Dictionary<QueryColumnListItem, string>> _dictionarySubQuery =
+            new Dictionary<UnionSubQuery, Dictionary<QueryColumnListItem, string>>();
 
         public CustomTextColumnDemoFrame()
-		{
-			InitializeComponent();
+        {
+            InitializeComponent();
+            queryBuilder1.ActiveUnionSubQueryChanging += QueryBuilder1_ActiveUnionSubQueryChanging;
+            queryBuilder1.ActiveUnionSubQueryChanged += QueryBuilder1_ActiveUnionSubQueryChanged;
+            queryBuilder1.QueryColumnListItemChanging += QueryBuilder1_QueryColumnListItemChanging;
 
-			// Fill query builder with demo data
-			queryBuilder1.SyntaxProvider = new MSSQLSyntaxProvider();
-			queryBuilder1.MetadataLoadingOptions.OfflineMode = true;
-			queryBuilder1.MetadataContainer.ImportFromXML("Northwind.xml");
-			queryBuilder1.InitializeDatabaseSchemaTree(); 
-			queryBuilder1.SQL = "select OrderID, CustomerID, OrderDate from Orders";
+            // Fill query builder with demo data
+            queryBuilder1.SyntaxProvider = new MSSQLSyntaxProvider();
+            queryBuilder1.MetadataLoadingOptions.OfflineMode = true;
+            queryBuilder1.MetadataContainer.ImportFromXML("Northwind.xml");
+            queryBuilder1.InitializeDatabaseSchemaTree();
 
-			// Fill custom values source (for demo purposes)
-			for (int i = 0; i < 100; i++)
-				_customValuesProvider.Add("Some Value " + i);
+            queryBuilder1.SQL = "select OrderID, CustomerID, OrderDate from Orders";
         }
 
-        private void queryBuilder1_QueryElementControlCreated(QueryElement queryElement, IQueryElementControl queryElementControl)
-		{
+        private void QueryBuilder1_QueryColumnListItemChanging(QueryColumnList queryColumnList, QueryColumnListItem queryColumnListItem, 
+            QueryColumnListItemProperty property, int conditionIndex, object oldValue, ref object newValue, ref bool abort)
+        {
+            
+        }
+
+        private static string CountCharacter(QueryColumnListItem value)
+        {
+            return string.IsNullOrEmpty(value.ExpressionString)
+                ? "Empty"
+                : $" Length {value.ExpressionString.Length}";
+        }
+
+        private void QueryBuilder1_ActiveUnionSubQueryChanged(object sender, System.EventArgs e)
+        {
+            if (queryBuilder1.ActiveUnionSubQuery == null) return;
+
+            var value = _dictionarySubQuery[queryBuilder1.ActiveUnionSubQuery];
+
+            foreach (var listItem in queryBuilder1.ActiveUnionSubQuery.QueryColumnList)
+            {
+                if (value.ContainsKey(listItem))
+                {
+                    value[listItem] = listItem.ExpressionString + CountCharacter(listItem);
+                    continue;
+                }
+
+                value.Add(listItem, listItem.ExpressionString + CountCharacter(listItem));
+            }
+
+            _dictionarySubQuery[queryBuilder1.ActiveUnionSubQuery] = value;
+        }
+
+        private void QueryColumnListOnElementRemoving(QueryElement sender, QueryElement child)
+        {
+            var queryColumnListItem = (QueryColumnListItem) child;
+            queryColumnListItem.Updated -= QueryColumnListItem_Updated;
+
+            if (_dictionarySubQuery.ContainsKey(queryBuilder1.ActiveUnionSubQuery))
+            {
+                if (_dictionarySubQuery[queryBuilder1.ActiveUnionSubQuery].ContainsKey(queryColumnListItem))
+                    _dictionarySubQuery[queryBuilder1.ActiveUnionSubQuery].Remove(queryColumnListItem);
+            }
+            else
+            {
+                foreach (var key in _dictionarySubQuery.Keys)
+                {
+                    if (!_dictionarySubQuery[key].ContainsKey(queryColumnListItem)) continue;
+                    
+                    _dictionarySubQuery[key].Remove(queryColumnListItem);
+                }
+            }
+        }
+
+        private void QueryColumnList_ElementAdded(QueryElement sender, QueryElement child)
+        {
+            var queryColumnListItem = (QueryColumnListItem) child;
+            queryColumnListItem.Updated += QueryColumnListItem_Updated;
+        }
+
+        private void QueryColumnListItem_Updated(object sender, System.EventArgs e)
+        {
+            var queryColumnListItem = (QueryColumnListItem)sender;
+            if (_dictionarySubQuery.ContainsKey(queryBuilder1.ActiveUnionSubQuery))
+            {
+                var values = _dictionarySubQuery[queryBuilder1.ActiveUnionSubQuery];
+
+                if (values.ContainsKey(queryColumnListItem))
+                    values[queryColumnListItem] =
+                        queryColumnListItem.ExpressionString + CountCharacter(queryColumnListItem);
+                else
+                    values.Add(queryColumnListItem,
+                        queryColumnListItem.ExpressionString + CountCharacter(queryColumnListItem));
+            }
+            else
+            {
+                _dictionarySubQuery.Add(queryBuilder1.ActiveUnionSubQuery, new Dictionary<QueryColumnListItem, string>
+                {
+                    {queryColumnListItem, queryColumnListItem.ExpressionString + queryColumnListItem.ExpressionString + CountCharacter(queryColumnListItem)}
+                });
+            }
+        }
+
+        private void QueryBuilder1_ActiveUnionSubQueryChanging(object sender,
+            ActiveQueryBuilder.View.SubQueryChangingEventArgs e)
+        {
+            if (e.OldSubQuery != null && _dictionarySubQuery.ContainsKey(e.OldSubQuery))
+            {
+                _dictionarySubQuery.Remove(e.OldSubQuery);
+                e.OldSubQuery.QueryColumnList.ElementAdded -= QueryColumnList_ElementAdded;
+                e.OldSubQuery.QueryColumnList.ElementRemoving -= QueryColumnListOnElementRemoving;
+            }
+
+            if (e.NewSubQuery == null || _dictionarySubQuery.ContainsKey(e.NewSubQuery)) return;
+
+            e.NewSubQuery.QueryColumnList.ElementAdded += QueryColumnList_ElementAdded;
+            e.NewSubQuery.QueryColumnList.ElementRemoving += QueryColumnListOnElementRemoving;
+
+
+            _dictionarySubQuery.Add(e.NewSubQuery, new Dictionary<QueryColumnListItem, string>());
+        }
+
+        private void queryBuilder1_QueryElementControlCreated(QueryElement queryElement,
+            IQueryElementControl queryElementControl)
+        {
             if (queryElementControl is IQueryColumnListControl)
             {
-                var queryColumnListControl = (IQueryColumnListControl)queryElementControl;
-                DataGridView dataGridView = (DataGridView)queryColumnListControl.DataGrid;
+                var queryColumnListControl = (IQueryColumnListControl) queryElementControl;
+                DataGridView dataGridView = (DataGridView) queryColumnListControl.DataGrid;
 
                 _customColumn?.Dispose();
 
@@ -63,13 +166,14 @@ namespace CustomColumnsDemo
                 dataGridView.CellValueNeeded += DataGridView_CellValueNeeded;
                 dataGridView.CellValuePushed += DataGridView_CellValuePushed;
             }
-		}
+        }
 
-		private void queryBuilder1_QueryElementControlDestroying(QueryElement queryElement, IQueryElementControl queryElementControl)
-		{
+        private void queryBuilder1_QueryElementControlDestroying(QueryElement queryElement,
+            IQueryElementControl queryElementControl)
+        {
             if (queryElementControl is IQueryColumnListControl)
             {
-                var queryColumnListControl = (IQueryColumnListControl)queryElementControl;
+                var queryColumnListControl = (IQueryColumnListControl) queryElementControl;
                 DataGridView dataGridView = (DataGridView) queryColumnListControl.DataGrid;
 
                 // remove event handlers to avoid memory leaking
@@ -77,51 +181,58 @@ namespace CustomColumnsDemo
                 dataGridView.CellValueNeeded -= DataGridView_CellValueNeeded;
                 dataGridView.CellValuePushed -= DataGridView_CellValuePushed;
             }
-		}
+        }
 
-		private void DataGridView_CellBeginEdit(object sender, DataGridViewCellCancelEventArgs e)
+        private void DataGridView_CellBeginEdit(object sender, DataGridViewCellCancelEventArgs e)
         {
             var dataGrid = (DataGridView) sender;
 
             if (dataGrid.Columns[e.ColumnIndex] != _customColumn) return;
 
-			if (e.RowIndex < ((DataGridView) sender).RowCount - 1)
-			{
-				// Make cell editable
-				e.Cancel = false; // Set true if you need read-only cell.			
-			}
-		}
+            if (e.RowIndex < ((DataGridView) sender).RowCount - 1)
+            {
+                // Make cell editable
+                e.Cancel = false; // Set true if you need read-only cell.			
+            }
+        }
 
-		// This event handler allows you to display some custom value in you column
-		private void DataGridView_CellValueNeeded(object sender, DataGridViewCellValueEventArgs e)
-		{
-            var dataGrid = (DataGridView)sender;
+        // This event handler allows you to display some custom value in you column
+        private void DataGridView_CellValueNeeded(object sender, DataGridViewCellValueEventArgs e)
+        {
+            var dataGrid = (DataGridView) sender;
 
             if (dataGrid.Columns[e.ColumnIndex] != _customColumn) return;
 
-            if (e.RowIndex < ((DataGridView) sender).RowCount - 1)
-			{
-				// Set cell value
-				e.Value = _customValuesProvider[e.RowIndex];
+            if (e.RowIndex >= ((DataGridView) sender).RowCount - 1) return;
+            Dictionary<QueryColumnListItem, string> data = _dictionarySubQuery[queryBuilder1.ActiveUnionSubQuery];
 
-				// If you need to access to the low level data item, use the following:
-//				QueryColumnListItem item = queryBuilder1.ActiveUnionSubQuery.QueryColumnList[e.RowIndex];
-			}
-		}
+            if (!data.ContainsKey(queryBuilder1.ActiveUnionSubQuery.QueryColumnList.Items[e.RowIndex])) return;
 
-		// This event handler allows you to store modified cell value (if your column is editable)
+            // Set cell value
+            e.Value = data[queryBuilder1.ActiveUnionSubQuery.QueryColumnList.Items[e.RowIndex]];
+
+            // If you need to access to the low level data item, use the following:
+            //QueryColumnListItem item = queryBuilder1.ActiveUnionSubQuery.QueryColumnList[e.RowIndex];
+        }
+
+        // This event handler allows you to store modified cell value (if your column is editable)
         private void DataGridView_CellValuePushed(object sender, DataGridViewCellValueEventArgs e)
         {
             var dataGrid = (DataGridView) sender;
 
             if (dataGrid.Columns[e.ColumnIndex] != _customColumn) return;
 
-            // Store new cell value
-            _customValuesProvider[e.RowIndex] = (string) e.Value;
+            if (e.RowIndex >= ((DataGridView) sender).RowCount - 1) return;
+            Dictionary<QueryColumnListItem, string> data = _dictionarySubQuery[queryBuilder1.ActiveUnionSubQuery];
+
+            if (data.ContainsKey(queryBuilder1.ActiveUnionSubQuery.QueryColumnList.Items[e.RowIndex]))
+            {
+                // Store new cell value
+                data[queryBuilder1.ActiveUnionSubQuery.QueryColumnList.Items[e.RowIndex]] = (string) e.Value;
+            }
 
             // If you need to access to the low level data item, use the following:
-//				QueryColumnListItem item = queryBuilder1.ActiveUnionSubQuery.QueryColumnList[e.RowIndex];
-
+            //QueryColumnListItem item = queryBuilder1.ActiveUnionSubQuery.QueryColumnList[e.RowIndex];
         }
     }
 }
